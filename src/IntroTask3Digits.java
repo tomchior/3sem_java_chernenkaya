51e29f57409e697c0a3bb1da6974929ba5517a99
Toma <toma.chornenkay@mail.ru>
2021-09-10 19:32:46 +0300

    IntroTask3Digits

---
+++ src/IntroTask3Digits.java
@@ -0,0 +1,12 @@
+public class IntroTask3Digits {
+    public static void main (String[] args){
+        int a = -999;
+        if (a >= 100 & a <= 999) {
+            System.out.println("true");
+        } else if ( a <= -100 & a >= -999){
+            System.out.println("true");
+        }else {
+            System.out.println("false");
+        }
+    }
+}
