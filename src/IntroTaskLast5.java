b8a7c56c5cf62291a18fb60c99cee52f63aa5f56
Toma <toma.chornenkay@mail.ru>
2021-09-15 22:09:32 +0300

    Вводная задача 2

---
+++ src/IntroTaskLast5.java
@@ -0,0 +1,12 @@
+public class IntroTaskLast5 {
+    public static void main(String[] args){
+        int a = 77;
+        if (a % 10 == 5){
+            System.out.println("На конце цифра 5");
+        }else if (a % 10 == -5) {
+            System.out.print("На конце цифра 5");
+        }else {
+            System.out.println("Ошибка");
+        }
+    }
+}
