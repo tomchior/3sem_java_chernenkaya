6b674ef840bf51a9cea07cef02c9578e1ba84b8a
Toma <toma.chornenkay@mail.ru>
2021-11-18 18:14:58 +0300

    исправила double mean

--- src/ArraysTasks.java
+++ src/ArraysTasks.java
@@ -4,14 +4,14 @@ import java.util.Arrays;
 public class ArraysTasks {
     public static void main(String[] args) {
         System.out.println(Arrays.toString(even(20)));
-        int[] array1 = {2,2,2,2,2};
-        int[] array2 = {2,3,4,15,25};
+        int[] array1 = {2, 2, 2, 2, 2};
+        int[] array2 = {2, 3, 4, 15, 25};
         System.out.println(allSimilar(array1));
         System.out.println(hasSimilar(array2));
-        System.out.println(mean(new int[]{1,2,3,45}));
+        System.out.println(mean(new int[]{10,10,10,10}));
         shift(array2);
         System.out.println(Arrays.toString(array2));
-        System.out.println(Arrays.toString(copyShift(new int[]{1,2,5,45,8,19,3})));
+        System.out.println(Arrays.toString(copyShift(new int[]{1, 2, 5, 45, 8, 19, 3})));
 
 
     }
@@ -20,16 +20,16 @@ public class ArraysTasks {
         int a = n;
         int[] numbers = new int[a];
         for (int i = 0; i < numbers.length; i++) {
-            if(numbers[i] % 2 == 0) {
-                numbers[i] = 2*(i+1);
+            if (numbers[i] % 2 == 0) {
+                numbers[i] = 2 * (i + 1);
             }
-            }
-        return numbers;
         }
+        return numbers;
+    }
 
 
     static boolean allSimilar(int[] a) {
-        if(a.length == 0) {
+        if (a.length == 0) {
             return true;
         } else {
             int first = a[0];
@@ -57,19 +57,19 @@ public class ArraysTasks {
 
     static double mean(int[] a) {
         double sum = 0;
-        for (int x: a) {
+        for (int x : a) {
             sum += x;
         }
-        return sum;
+        return sum / a.length;
     }
 
     static void shift(int[] a) {
         int[] b = new int[a.length];
-        b[0] = a[a.length-1];
-        for(int i = 1; i < a.length; i++) {
-                b[i] = a[i-1];
-            }
-        for(int i = 0; i < a.length; i++) {
+        b[0] = a[a.length - 1];
+        for (int i = 1; i < a.length; i++) {
+            b[i] = a[i - 1];
+        }
+        for (int i = 0; i < a.length; i++) {
             a[i] = b[i];
         }
     }
@@ -77,10 +77,10 @@ public class ArraysTasks {
 
     static int[] copyShift(int[] a) {
         int[] b = new int[a.length];
-        b[0] = a[a.length-1];
-            for(int i = 1; i < b.length; i++) {
-                b[i] = a[i-1];
-            }
+        b[0] = a[a.length - 1];
+        for (int i = 1; i < b.length; i++) {
+            b[i] = a[i - 1];
+        }
         return b;
     }
 
