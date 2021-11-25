9c343bcf276f9584dd7ff37f97a5ae92856a8c6a
Toma <toma.chornenkay@mail.ru>
2021-11-25 18:43:31 +0300

    Algorithms intPrimes

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -2,19 +2,19 @@ import java.util.Arrays;
 
 public class Algorithms {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(sieveOfEratosthenes(30)));
+        System.out.println(Arrays.toString(sieveOfEratosthenes(25)));
+        System.out.println(Arrays.toString(primes(20)));
     }
 
     static boolean[] sieveOfEratosthenes(int n) {
-        int a = n + 1;
-        boolean[] prime = new boolean[a];
+        boolean[] prime = new boolean[n + 1];
         Arrays.fill(prime, true);
         prime[0] = false;
         prime[1] = false;
 
-        for (int i = 2; i * i < n; i++) {
+        for (int i = 2; i * i < n + 1; i++) {
             if (prime[i]) {
-                for (int j = i * i; j < n; j += i) {
+                for (int j = 2 * i; j < n + 1; j += i) {
                     prime[j] = false;
                 }
             }
@@ -22,7 +22,22 @@ public class Algorithms {
         return prime;
     }
 
-
+    static int[] primes(int n) {
+        boolean[] prime = sieveOfEratosthenes(n);
+        int num = 0;
+        for (boolean x : prime)
+            if(x)
+               num++;
+        int[] arr = new int[num];
+        int j = 0;
+        for (int i = 0; i < n; i++) {
+            if (prime[i]) {
+                arr[j] = i;
+                j++;
+            }
+        }
+        return arr;
+    }
 }
 
 
