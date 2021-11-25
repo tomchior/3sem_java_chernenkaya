1eb63961c5883fe08f4cd0766acd7c0f58c9909f
Toma <toma.chornenkay@mail.ru>
2021-11-25 18:42:20 +0300

    Algorithms intPrimes

--- src/PrimalityTest.java
+++ src/PrimalityTest.java
@@ -1,13 +1,13 @@
 public class PrimalityTest {
     public static void main(String[] args) {
-        System.out.println(NumSimple(25));
-    }
+    System.out.println(NumSimple(20));
+   }
 
     public static boolean NumSimple(int n) {
-        for (int i=2; i < n; i++) {
-            if( n%i == 0 ) {
-                return false;
-            }
+      for (int i=2; i < n; i++) {
+        if( n%i == 0 ) {
+          return false;
+         }
         }
         return true;
     }
